6c46546d787bf8e283bc4a4d3b3ada3728ccf192
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-04-10 16:10:08 -0300

    Adiciona operador ternário

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 var speed: int = 400
+var is_walking: bool = false
 var is_running: bool = false
 
 func get_movement()-> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	is_running = false if velocity == Vector2(0, 0) else true 
 
 func get_accel_input()-> void:
 	if Input.is_action_just_pressed("shift"):
@@ -17,7 +19,7 @@ func get_accel_input()-> void:
 		print("WALKING!")
 		speed = 400
 
-func _physics_process(delta: float):
+func _physics_process(_delta: float):
 	get_movement()
 	get_accel_input()
 	move_and_slide()
