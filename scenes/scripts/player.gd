6cd46390a6cba6fd9d1be0704418647ae2429143
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-04-10 16:05:35 -0300

    Starts character movement

---
+++ scenes/scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed: int = 400
+var is_running: bool = false
+
+func get_movement()-> void:
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func get_accel_input()-> void:
+	if Input.is_action_just_pressed("shift"):
+		is_running = true
+		print("RUNNING!")
+		speed = 600
+	if Input.is_action_just_released("shift"):
+		is_running = false
+		print("WALKING!")
+		speed = 400
+
+func _physics_process(delta: float):
+	get_movement()
+	get_accel_input()
+	move_and_slide()
